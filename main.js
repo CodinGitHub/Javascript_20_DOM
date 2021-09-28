4374c9730c30a7e687c4c8541223801dabec738d
CodinGitHub <gms2programador@gmail.com>
2021-09-28 06:15:51 -0500

    Seleccionando elementos

---
+++ main.js
@@ -0,0 +1,14 @@
+// SELECCIONANDO ELEMENTOS
+
+//Por ID
+let elemento1 = document.getElementById('parrafo1');
+elemento1.style.color = '#FF00FF';
+
+//Por clase
+let elemento2 = document.getElementsByClassName('parrafos');
+contenedor.style.fontWeight = 800;
+
+//Por etiqueta
+let elemento3 = document.getElementsByTagName('p')[2]
+console.log(elemento3);
+elemento3.style.color = '#FF00FF';
